feb0013fa17fa1238dc5e0d0af49fddf7d69d2d0
Midori-Funaki <funamoru@gmail.com>
2018-01-19 19:12:31 +0800

    Edit routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -8,9 +8,9 @@ class UserRoutes{
   router(){
     let router = express.Router();
     router.get('/:id', this.get.bind(this));
-    router.post('/register',this.post.bind(this));
-    router.delete('/deregister/:id',this.delete.bind(this));
-    router.post('/update/:id',this.update.bind(this));
+    router.post('/',this.post.bind(this));
+    router.delete('/:id',this.delete.bind(this));
+    router.put('/:id',this.update.bind(this));
     return router;
   }
 
