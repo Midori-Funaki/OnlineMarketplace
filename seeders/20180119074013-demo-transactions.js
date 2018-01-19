af91a04101d60e543ce819e3dc42ffeeb8b06ca7
Midori-Funaki <funamoru@gmail.com>
2018-01-19 18:37:56 +0800

    Transactions seed updated

--- seeders/20180119074013-demo-transactions.js
+++ seeders/20180119074013-demo-transactions.js
@@ -2,26 +2,22 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-      return queryInterface.bulkInsert('Person', [{
-        status: DataTypes.STRING,
-        price: DataTypes.FLOAT,
-        sellerShipAddress: DataTypes.STRING,
-        sellerBillAddress: DataTypes.STRING,
-        buyerShipAddress: DataTypes.STRING,
-        buyerBillAddress: DataTypes.STRING,
-        buyerId: DataTypes.INTEGER,
-        sellerId: DataTypes.INTEGER,
-        productId: DataTypes.INTEGER
-      }], {});
+    return queryInterface.bulkInsert('Transactions', [{
+      status: 1,
+      price: 650,
+      sellerShipAddress: "70 Bowman St., South Windsor, CT 06074",
+      sellerBillAddress: "70 Bowman St., South Windsor, CT 06074",
+      buyerShipAddress: "4 Goldfield Rd., Honolulu, HI 96815",
+      buyerBillAddress: "4 Goldfield Rd., Honolulu, HI 96815",
+      buyerId: 15,
+      sellerId: 14,
+      productId: 7,
+      createdAt: "2016-08-09 07:42:28",
+      updatedAt: "2016-08-09 07:42:28"
+    }], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+    return queryInterface.bulkDelete('Transactions', null, {});
   }
 };
