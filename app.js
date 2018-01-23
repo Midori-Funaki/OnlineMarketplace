594203f3ea0969289e8d1ee1037ef67b6423be52
Adrian Lee <alee.lcagency@gmail.com>
2018-01-23 10:55:40 +0800

    frontend added

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ var productService = new ProductService(path);
 var transactionService = new TransactionService(path);
 var cartService = new CartService(path);
 
+//frontend imports
+var reload = require('reload');
+var watch = require('watch');
+var jwt = require('jwt-simple');
+var axios = require('axios');
+var authClass = require('./auth');
+var config = require('./config');
+var users = require('./users');
+
 var app = express();
 
 // view engine setup
@@ -40,16 +49,112 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+// Authentication Login - switched to somewhere else later on
+app.post("/api/login", function(req, res) {  
+    console.log(req.body.username)
+    if (req.body.username && req.body.password) {
+        var username = req.body.username;
+        var password = req.body.password;
+        var user = users.find((u)=> {
+            return u.name === username && u.password === password;
+        });
+        if (user) {
+            var payload = {
+                id: user.id
+            };
+            var token = jwt.encode(payload, config.jwtSecret);
+            res.json({
+                token: token
+            });
+        } else {
+            res.sendStatus(401);
+        }
+    } else {
+        res.sendStatus(401);
+  
+    }
+  });
+  
+app.post("/api/login/facebook", function(req, res) {  
+if (req.body.access_token) {
+    var accessToken = req.body.access_token;
+    
+    axios.get(`https://graph.facebook.com/me?access_token=${accessToken}&fields=id,email,name`)
+    .then((data)=>{
+        console.log(data.data);
+        if(!data.data.error){
+            var payload = {
+                id: accessToken
+            };
+            var token = jwt.encode(payload, config.jwtSecret);
+            res.json({
+                token: token
+            });
+        }else{
+            res.sendStatus(401);
+        }
+    }).catch((err)=>{
+        console.log(err);
+        res.sendStatus(401);
+    });
+} else {
+    res.sendStatus(401);
+
+}
+});
+
+app.post("/api/login/google", function(req, res) {  
+if (req.body.access_token) {
+    var accessToken = req.body.access_token;
+    
+    axios.get(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`)
+    .then((data)=>{
+        if(!data.data.error){
+            var payload = {
+                id: accessToken
+            };
+            console.log(data.data);
+            var token = jwt.encode(payload, config.jwtSecret);
+            res.json({
+                token: token
+            });
+        }else{
+            res.sendStatus(401);
+        }
+    }).catch((err)=>{
+        console.log(err);
+        res.sendStatus(401);
+    });
+} else {
+    res.sendStatus(401);
+
+}
+});
+
+// Reloading the backend when backend is changed.
+let reloadServer = reload(app);
+
+watch.watchTree(__dirname + "/frontend/dist", function (f, curr, prev) {
+    // Fire server-side reload event 
+    reloadServer.reload();
+});
 
 // Routing
-app.use('/', index);
+//app.use('/', index);
 app.use('/api/users/:id/products', new UserProductRoutes(userService).router());
 app.use('/api/users',new UserRoutes(userService).router().use('/api/users/:id/products', new UserProductRoutes(userService).router()));
 app.use('/api/transactions',new TransactionRoutes(transactionService).router({mergeParams: true}));
 app.use('/api/products', new ProductRoutes(productService).router());
 app.use('/api/categories', new CategoryRoutes(categoryService).router());
 app.use('/api/carts', new CartRoutes(cartService).router());
+
+app.use(function(req, res, next) {
+    res.sendFile(__dirname + "/frontend/dist/index.html");
+})
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -68,5 +173,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+app.listen(8080);
+
 
 module.exports = app;
--- omp-frontonly/frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  //Put your facebook App ID below
-  facebookAppId : "156841828428687",
-  googleAppId: "881529039427-tgg1b372td7mlu6tst3n0toha4h4m83v.apps.googleusercontent.com"
-};
